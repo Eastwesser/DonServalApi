a06e28be1429eec9f83bde9f0dec885375e7af0e
Dennis Mattews <alttert@mail.ru>
2024-09-17 01:09:30 +0300

    improved readability

--- api/core/db_helper.py
+++ api/core/db_helper.py
@@ -6,7 +6,11 @@ from api.core.config import settings
 class DatabaseHelper:
     def __init__(self, url: str, echo: bool = False):
         self.engine = create_async_engine(url, echo=echo)
-        self.session_factory = async_sessionmaker(bind=self.engine, autoflush=False, autocommit=False)
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+        )
 
     async def session_dependency(self):
         async with self.session_factory() as session:
--- api/donuts/views/views.py
+++ api/donuts/views/views.py
@@ -16,32 +16,48 @@ os.makedirs(IMAGE_DIR, exist_ok=True)
 
 
 @router.post("/", response_model=DonutRead)
-async def create_donut_endpoint(donut: DonutCreate, db: AsyncSession = Depends(db_helper.session_dependency)):
+async def create_donut_endpoint(
+        donut: DonutCreate,
+        db: AsyncSession = Depends(db_helper.session_dependency)
+):
     return await create_donut(db, donut)
 
 
 @router.get("/{donut_id}", response_model=DonutRead)
-async def read_donut(donut_id: int, db: AsyncSession = Depends(db_helper.session_dependency)):
+async def read_donut(
+        donut_id: int,
+        db: AsyncSession = Depends(db_helper.session_dependency)
+):
     return await get_donut(db, donut_id)
 
 
 @router.put("/{donut_id}", response_model=DonutRead)
-async def update_donut_endpoint(donut_id: int, donut: DonutUpdate,
-                                db: AsyncSession = Depends(db_helper.session_dependency)):
+async def update_donut_endpoint(
+        donut_id: int,
+        donut: DonutUpdate,
+        db: AsyncSession = Depends(db_helper.session_dependency)
+):
     return await update_donut(db, donut_id, donut)
 
 
 @router.delete("/{donut_id}")
-async def delete_donut_endpoint(donut_id: int, db: AsyncSession = Depends(db_helper.session_dependency)):
+async def delete_donut_endpoint(
+        donut_id: int,
+        db: AsyncSession = Depends(db_helper.session_dependency)
+):
     await delete_donut(db, donut_id)
     return {"message": "Donut deleted successfully"}
 
 
 @router.post("/{donut_id}/upload-image/")
-async def upload_image(donut_id: int, file: UploadFile = File(...),
-                       db: AsyncSession = Depends(db_helper.session_dependency)):
+async def upload_image(
+        donut_id: int,
+        file: UploadFile = File(...),
+        db: AsyncSession = Depends(db_helper.session_dependency)
+):
     db_donut = await get_donut(db, donut_id)
     file_location = f"{IMAGE_DIR}/{donut_id}.png"
+
     with open(file_location, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     db_donut.image_filename = file_location
@@ -51,7 +67,10 @@ async def upload_image(donut_id: int, file: UploadFile = File(...),
 
 
 @router.get("/{donut_id}/image")
-async def get_image(donut_id: int, db: AsyncSession = Depends(db_helper.session_dependency)):
+async def get_image(
+        donut_id: int,
+        db: AsyncSession = Depends(db_helper.session_dependency)
+):
     db_donut = await get_donut(db, donut_id)
     if db_donut.image_filename is None:
         raise HTTPException(status_code=404, detail="Image not found")
