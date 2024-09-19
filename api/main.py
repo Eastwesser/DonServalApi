d5cd2c414aa789908369eaeb2cd235c897e16faf
Dennis Mattews <alttert@mail.ru>
2024-09-19 06:33:17 +0300

    main.py upd

--- api/main.py
+++ api/main.py
@@ -3,14 +3,13 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
-from api.core.config import settings
-from api.core.models import Base
 from api.core.db_helper import db_helper
+from api.core.models import Base
 from api.donuts.views.views import router as donuts_router
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(_: FastAPI):
     async with db_helper.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
@@ -20,10 +19,15 @@ app = FastAPI(
     title="DonServal API",
     description="Serval Donut API for the donut shop",
     version="1.0.0",
-    lifespan=lifespan
+    lifespan=lifespan,
 )
 
 app.include_router(donuts_router, prefix="/donuts")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
