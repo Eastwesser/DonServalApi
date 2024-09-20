12df94cd56611ca0a94ee793a7782260389bba6e
Dennis Mattews <alttert@mail.ru>
2024-09-20 11:01:17 +0300

    database_test.py improved readability

--- tests/database_test.py
+++ tests/database_test.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import sessionmaker
 from tests.config_test import TestConfig
 
 test_engine = create_engine(TestConfig.TEST_DB_URL)
-TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
+TestSessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=test_engine,
+)
 
 
 def get_test_db():
