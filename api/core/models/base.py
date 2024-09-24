4f5777a69e58126e8505f7c6f9aec3c7e5efe07c
Денис Матвеев <alttert@mail.ru>
2024-09-24 12:00:00 +0300

    base comments upd

--- api/core/models/base.py
+++ api/core/models/base.py
@@ -3,4 +3,5 @@ from sqlalchemy.orm import DeclarativeBase
 
 # Define the base class for all SQLAlchemy models in the project.
 class Base(DeclarativeBase):
-    __abstract__ = True  # This marks the class as abstract, meaning no table will be created for this class itself.
+    __abstract__ = True  # This marks the class as abstract,
+    # meaning no table will be created for this class itself.
